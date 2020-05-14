91ad92c02465b4b11c182620d81c14c29beddcbb
Markus Merklinger <markus.merklinger@code.berlin>
2020-05-14 14:25:54 +0200

    Various changes to export data
    * Updated list of books in `all.csv`.
    * Removed designation based csv and sql files.
    * Removed `execute.sh`, because not longer required.
    * Updated `convert.py` to output sql into `all.sql`, instead of printing on stdout. Also assignes ids for book covers, creates the list `book_cover.csv` and downloads them to the `book_cover/` directory.

--- export/convert.py
+++ export/convert.py
@@ -1,4 +1,6 @@
 import sys, csv, re
+from pathlib import Path
+import urllib.request
 
 class Book:
   def __init__(self, row):
@@ -93,40 +95,51 @@ class Book:
     # copies
     self.copies = int(row[11])
 
-  def generate_sql(self):
+  def generate_sql(self, f_sql):
     # publishers
-    print("INSERT INTO publishers (name) SELECT '{}' WHERE NOT EXISTS (SELECT * FROM publishers WHERE name = '{}');".format(self.publisher, self.publisher))
+    f_sql.write("INSERT INTO publishers (name) SELECT '{}' WHERE NOT EXISTS (SELECT * FROM publishers WHERE name = '{}');\n".format(self.publisher, self.publisher))
 
     # subject_areas
     for i in self.subject_area:
-      print("INSERT INTO subject_areas (name) SELECT '{}' WHERE NOT EXISTS (SELECT * FROM subject_areas WHERE name = '{}');".format(i, i))
+      f_sql.write("INSERT INTO subject_areas (name) SELECT '{}' WHERE NOT EXISTS (SELECT * FROM subject_areas WHERE name = '{}');\n".format(i, i))
 
     # authors
     for i in self.authors_editors:
-      print("INSERT INTO persons (first_name, last_name) SELECT '{}', '{}' WHERE NOT EXISTS (SELECT * FROM persons WHERE first_name = '{}' AND last_name = '{}');".format(i[0], i[1], i[0], i[1]))
+      f_sql.write("INSERT INTO persons (first_name, last_name) SELECT '{}', '{}' WHERE NOT EXISTS (SELECT * FROM persons WHERE first_name = '{}' AND last_name = '{}');\n".format(i[0], i[1], i[0], i[1]))
 
     # books
-    print("INSERT INTO books \
+    f_sql.write("INSERT INTO books \
       (isbn_13, issn, title, subtitle, description, cover, edition, release_year, pages, code_identifier, publisher_id, designation_id, series_id, language_id, physical_size_id) \
       VALUES \
-      ('{}', {}, '{}', '{}', {}, {}, {}, {}, {}, '{}', {}, {}, {}, {}, {});"\
+      ('{}', {}, '{}', '{}', {}, {}, {}, {}, {}, '{}', {}, {}, {}, {}, {});\n"\
       .format(self.isbn_13, self.issn, self.title, self.subtitle, self.description, self.cover, self.edition, self.release_year, self.pages, self.code_identifier, "(SELECT id FROM publishers WHERE name = '{}')".format(self.publisher), "(SELECT id FROM designations WHERE name = '{}')".format(self.designation), self.series, "(SELECT id FROM languages WHERE iso_code = '{}')".format(self.language), "(SELECT id FROM physical_sizes WHERE name = '{}')".format(self.physical_size)))
 
     # copies
-    print("INSERT INTO copies (book_id, status_id) VALUES ({}, 1);".format("(SELECT id FROM books WHERE title = '{}' AND subtitle = '{}')".format(self.title, self.subtitle)))
+    f_sql.write("INSERT INTO copies (book_id, status_id) VALUES ({}, 1);\n".format("(SELECT id FROM books WHERE title = '{}' AND subtitle = '{}')".format(self.title, self.subtitle)))
 
     # authors -> books
     for i in self.authors_editors:
-      print("INSERT INTO books_authors (book_id, person_id) VALUES ({}, {});".format("(SELECT id FROM books WHERE title = '{}' AND subtitle = '{}')".format(self.title, self.subtitle), "(SELECT id FROM persons WHERE first_name = '{}' AND last_name = '{}')".format(i[0], i[1])))
+      f_sql.write("INSERT INTO books_authors (book_id, person_id) VALUES ({}, {});\n".format("(SELECT id FROM books WHERE title = '{}' AND subtitle = '{}')".format(self.title, self.subtitle), "(SELECT id FROM persons WHERE first_name = '{}' AND last_name = '{}')".format(i[0], i[1])))
 
     # subject_areas -> books
     for i in self.subject_area:
-      print("INSERT INTO books_subject_areas (book_id, subject_area_id) VALUES ({}, {});".format("(SELECT id FROM books WHERE title = '{}' AND subtitle = '{}')".format(self.title, self.subtitle), "(SELECT id FROM subject_areas WHERE name = '{}')".format(i)))
+      f_sql.write("INSERT INTO books_subject_areas (book_id, subject_area_id) VALUES ({}, {});\n".format("(SELECT id FROM books WHERE title = '{}' AND subtitle = '{}')".format(self.title, self.subtitle), "(SELECT id FROM subject_areas WHERE name = '{}')".format(i)))
 
 
 if __name__ == '__main__':
+  book_cover_id = 0
+  Path("book_cover").mkdir(parents=True, exist_ok=True)
   with open(sys.argv[1], 'r') as f_csv:
-    next(f_csv)
-    for row in csv.reader(f_csv, delimiter=',', quotechar='"'):
-      book = Book(row)
-      book.generate_sql()
+    with open('all.sql', 'a') as f_sql:
+      with open('book_cover.csv', 'a') as f_book_cover:
+        for row in csv.reader(f_csv, delimiter=',', quotechar='"'):
+          book = Book(row)
+          book.cover = book_cover_id
+          book.generate_sql(f_sql)
+          f_book_cover.write('{},"{}"\n'.format(book_cover_id, row[5]))
+          try:
+            urllib.request.urlretrieve(row[5], "book_cover/{}.jpg".format(book_cover_id))
+          except:
+              print("Error: Failed to fetch cover for book cover id {}".format(book_cover_id))
+          book_cover_id += 1
+
--- export/execute.sh
+++
@@ -1,7 +0,0 @@
-python convert.py library_dh.csv > library_dh.sql
-python convert.py library_id.csv > library_id.sql
-python convert.py library_is.csv > library_is.sql
-python convert.py library_pm.csv > library_pm.sql
-python convert.py library_se.csv > library_se.sql
-python convert.py library_sts.csv > library_sts.sql
-python convert.py library_xxl.csv > library_xxl.sql
